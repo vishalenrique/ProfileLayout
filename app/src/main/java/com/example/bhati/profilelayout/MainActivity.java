785ba2bde32277f7b136c50cf5e1f946779f14f9
vishal <vishal3bhati@gmail.com>
2018-03-31 14:36:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bhati/profilelayout/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.bhati.profilelayout;
+
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.jaeger.library.StatusBarUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //ImageView coverImage = findViewById(R.id.coverImage);
+       // StatusBarUtil.setColor(this, ContextCompat.getColor(this,R.color.colorAccent));
+       // StatusBarUtil.setTranslucent(this);
+       // StatusBarUtil.setTransparent(this);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow(); // in Activity's onCreate() for instance
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+
+
+    }
+}
