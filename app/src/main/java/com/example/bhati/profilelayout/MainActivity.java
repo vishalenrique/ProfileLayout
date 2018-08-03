42850f195cd63550f23ba8702c763008b132dbfd
vishal <vishal3bhati@gmail.com>
2018-08-03 23:08:02 +0530

    Add screenshots

--- app/src/main/java/com/example/bhati/profilelayout/MainActivity.java
+++ app/src/main/java/com/example/bhati/profilelayout/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.bhati.profilelayout;
 
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
